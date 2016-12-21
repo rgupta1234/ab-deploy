115c05cf30bfb2535f95b73bc3b8cc4f2927b406
rgupta1234 <rgupta@redhat.com>
2016-12-21 17:07:22 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
  <?php 
- echo "I am VERSION 2 <br><br>";
+ echo "Welcome American Airlines (Worlds largest by the way :)) to Openshift!!<br><br>";
  echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'] ;
  ?>
